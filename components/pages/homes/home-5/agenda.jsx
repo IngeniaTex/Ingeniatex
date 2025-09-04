404814660753978687e2e7a5bab1eee3fe08d674
Omar Yepez <ypz.omar@gmail.com>
2025-09-03 18:12:51 -0600

    added booking link

---
+++ components/pages/homes/home-5/agenda.jsx
@@ -0,0 +1,19 @@
+
+
+const Agenda = () => {
+    return (
+        <div className="container py-10">
+        <h1 className="text-3xl font-semibold mb-6">Agenda una reunión</h1>
+        <a
+          href="https://calendar.app.google/QuZ6YeFf5u3HDSZT9"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn-one"
+        >
+          Reservar en mi calendario
+        </a>
+      </div>
+    );
+};
+
+export default Agenda;
